245f68f337e3bde3cd09ab83dd85901c66b7b477
andikaryan <92864047+andikaryan@users.noreply.github.com>
2022-09-26 07:29:51 +0700

    Latian 1

---
+++ Latian1/Latian1/Laptop.cs
@@ -0,0 +1,89 @@
+﻿using iniProcessor;
+using iniVga;
+
+class Laptop
+{
+    public string merk;
+    public string tipe;
+    public Vga vga;
+    public Processor processor;
+    public void LaptopDinyalakan()
+    {
+        Console.WriteLine($"Laptop {merk} {tipe} menyala");
+    }
+    public void LaptopDimatikan()
+    {
+        Console.WriteLine($"Laptop {merk} {tipe} mati");
+    }
+}
+class ASUS : Laptop
+{
+    public ASUS()
+    {
+        merk = "ASUS";
+    }
+}
+class ROG : ASUS
+{
+    public ROG()
+    {
+        tipe = "ROG";
+    }
+}
+class Vivobook : ASUS
+{
+    public Vivobook()
+    {
+        tipe = "Vivobook";
+    }
+    public void Ngoding()
+    {
+        Console.WriteLine("Ctak Ctak Ctak, error lagi!!");
+    }
+}
+class ACER : Laptop
+{
+    public ACER()
+    {
+        merk = "ACER";
+    }
+}
+class Swift : ACER
+{
+    public Swift()
+    {
+        tipe = "Swift";
+    }
+}
+class Predator : ACER
+{
+    public Predator()
+    {
+        tipe = "Predator";
+    }
+    public void BermainGame()
+    {
+        Console.WriteLine($"Laptop {this.merk} {this.tipe} sedang memainkan game");
+    }
+}
+class Lenovo : Laptop
+{
+    public Lenovo()
+    {
+        merk = "Lenovo";
+    }
+}
+class IdeaPad : Lenovo
+{
+    public IdeaPad()
+    {
+        tipe = "IdeaPad";
+    }
+}
+class Legion : Lenovo
+{
+    public Legion()
+    {
+        tipe = "Legion";
+    }
+}
---
+++ Latian1/Latian1/Program.cs
@@ -0,0 +1,44 @@
+﻿using iniProcessor;
+using iniVga;
+
+namespace Latian1
+{
+    internal class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("[SYSTEM] BERHASIL MEMBUAT OBJECT");
+            Console.ResetColor();
+
+            Laptop laptop1 = new Vivobook();
+            Laptop laptop2 = new IdeaPad();
+            Predator predator = new Predator();
+
+            ////soal nomer 1
+            ////memanggil method
+            //laptop2.LaptopDimatikan();
+            //laptop2.LaptopDinyalakan();
+
+            ////Soal nomer 2
+            ////Memanggil method
+            //laptop1.Ngoding();
+
+            ////Soal nomer 3
+            //laptop1.vga = new Nvidia();
+            //laptop1.processor = new CoreI5();
+            //Console.WriteLine("Merk Vga: " + laptop1.vga.merk +
+            //    "\n" + "Merk Processor: " + laptop1.processor.merk +
+            //    "\n" + "Tipe Processor: " + laptop1.processor.tipe);
+
+            ////Soal nomer 4
+            ////Memanggil method
+            //predator.BermainGame();
+
+            ////Soal nomer 5
+            //ACER acer = new Predator();
+            ////Memanggil method
+            //acer.BermainGame();
+        }
+    }
+}
